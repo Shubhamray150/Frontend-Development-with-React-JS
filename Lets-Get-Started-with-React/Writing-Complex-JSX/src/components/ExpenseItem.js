8853a113a250282a06931be2428213a4c822a5d6
Shubham Ray <shubhamray150@gmail.com>
2025-06-14 15:01:44 +0530

    adding writing-complex-jsx file

---
+++ Lets-Get-Started-with-React/Writing-Complex-JSX/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+// Write your code here:
+export default function ExpenseItem() {
+  return (
+    <>
+      <div>15 August 2023</div>
+      <div>Book</div>
+      <div>$10</div>
+      <div>Delhi</div>
+    </>
+  )
+}
