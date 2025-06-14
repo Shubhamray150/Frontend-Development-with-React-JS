e351a29c410ae959bc64c8aedbb72de0559bfba9
Shubham Ray <shubhamray150@gmail.com>
2025-06-14 14:33:42 +0530

    made changes in project setup folder and completed the task

--- Lets-Get-Started-with-React/project-setup/src/App.js
+++ Lets-Get-Started-with-React/project-setup/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Write your code here:
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>I am learning React</p>
+      <h1>Let's get Started</h1>
+    </>
   );
 }
-
 export default App;
