8d2dfd81654c024b4108e52893665e7351c7992c
Shubham Ray <shubhamray150@gmail.com>
2025-06-15 23:08:02 +0530

    add splitting the components folder

---
+++ Lets-Get-Started-with-React/09-Splitting-the-Components/src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+// Write your code at relevant places in the code below
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
