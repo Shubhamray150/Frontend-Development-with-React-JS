afcb03ca795ea9c398e27cab6976b0d2ed3f9e96
Shubham Ray <shubhamray150@gmail.com>
2025-07-10 09:26:48 +0530

    rendered userInputData on window and added rendering expense folder to git

---
+++ 02-Advance React/08-Rendering-Expenses/src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const data = { ...enteredExpenseData, id: Math.random().toString() };
+    props.onAddExpense(data);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
