a4adde5e5e5f8ccd8c4f6bf00dd4c92aca6de921
Shubham Ray <shubhamray150@gmail.com>
2025-07-11 12:16:57 +0530

    learnt custom hooks

---
+++ 02-Advance React/14-Custom-Hook-in-React/src/hooks/useFetch.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setError(null);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+
+  // Return the data, loading, and error states
+};
+
+export default useFetch;
