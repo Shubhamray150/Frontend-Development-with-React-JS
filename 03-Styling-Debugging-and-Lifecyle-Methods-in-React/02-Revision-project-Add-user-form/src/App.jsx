58bb9bbb131bd127b934523020549cc0654e9f66
Shubham Ray <shubhamray150@gmail.com>
2025-07-19 20:04:45 +0530

    created Revision project and added userform

---
+++ 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/App.jsx
@@ -0,0 +1,14 @@
+// Write your code at relevant places in the code below:
+
+import React from "react";
+import AddUser from "./component/Users/AddUser";
+
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
