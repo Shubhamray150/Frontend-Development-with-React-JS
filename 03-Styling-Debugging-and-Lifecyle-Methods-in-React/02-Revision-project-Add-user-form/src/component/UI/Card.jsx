d6d21eb3f0cc0154cb9aec2c800fb26618c63d14
Shubham Ray <shubhamray150@gmail.com>
2025-07-19 20:38:56 +0530

    created card component

---
+++ 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/component/UI/Card.jsx
@@ -0,0 +1,7 @@
+// Write your code below:
+import "./Card.css";
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+export default Card;
