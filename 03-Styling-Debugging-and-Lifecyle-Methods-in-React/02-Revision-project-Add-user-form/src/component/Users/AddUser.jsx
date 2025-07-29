f68d6bb67e5485215e8fe6366ee13cd4260024e2
Shubham Ray <shubhamray150@gmail.com>
2025-07-29 19:36:40 +0530

    added card and updated the ui with css

--- 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/component/Users/AddUser.jsx
+++ 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/component/Users/AddUser.jsx
@@ -1,18 +1,27 @@
-// Write your code below:
+// Write your code at relevant places in the code below:
+
 import React from "react";
+import "./AddUser.css";
+import Card from "../UI/Card";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" />
-      <label htmlFor="age">Age</label>
-      <input id="age" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className="input">
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
+
 export default AddUser;
