7d44b438a89002eaa56e2a5ab1704765285d3f81
Shubham Ray <shubhamray150@gmail.com>
2025-07-29 20:12:38 +0530

    collecting userInput and consoling that

--- 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/component/Users/AddUser.jsx
+++ 03-Styling-Debugging-and-Lifecyle-Methods-in-React/02-Revision-project-Add-user-form/src/component/Users/AddUser.jsx
@@ -1,22 +1,42 @@
-import React from "react";
-import "./AddUser.css";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import "./AddUser.css";
 
 const AddUser = () => {
-  const addUserHandler = (event) => {
-    event.preventDefault();
+  const [usernameInput, setUsernameInput] = useState("");
+  const [ageInput, setAgeInput] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setUsernameInput(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setAgeInput(event.target.value);
   };
 
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(usernameInput, ageInput);
+    setUsernameInput("");
+    setAgeInput("");
+  };
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-
+        <input
+          id="username"
+          type="text"
+          value={usernameInput}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
-
+        <input
+          id="age"
+          value={ageInput}
+          type="number"
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
