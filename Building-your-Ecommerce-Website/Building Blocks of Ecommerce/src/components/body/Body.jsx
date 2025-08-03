7d6695585e4b6d50aca8731cbcb5e2c9445f27be
Shubham Ray <shubhamray150@gmail.com>
2025-08-03 11:37:37 +0530

    implementing feature

--- Building-your-Ecommerce-Website/Building Blocks of Ecommerce/src/components/body/Body.jsx
+++ Building-your-Ecommerce-Website/Building Blocks of Ecommerce/src/components/body/Body.jsx
@@ -4,21 +4,25 @@ import "./Body.css";
 
 const productsArr = [
   {
+    album: "Album 1",
     title: "Colors",
     price: 100,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
   },
   {
+    album: "Album 2",
     title: "Black and white Colors",
     price: 50,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
   },
   {
+    album: "Album 3",
     title: "Yellow and Black Colors",
     price: 70,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
   },
   {
+    album: "Album 4",
     title: "Blue Color",
     price: 100,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
@@ -35,9 +39,27 @@ const Body = () => {
       }}
     >
       <Container className="text-center">
-        <div className="mb-4">
-          <h2 className="metal-title">MUSIC</h2>
-        </div>
+        <h2 className="mb-4 fw-bold">MUSIC</h2>
+        <Row>
+          {productsArr.map((product, index) => (
+            <Col key={index} xs={12} sm={6} md={3} className="mb-4">
+              <Card className="h-100">
+                <Card.Img variant="top" src={product.imageUrl} />
+                <Card.Body className="d-flex flex-column justify-content-between">
+                  <Card.Title className="fw-semibold">
+                    {product.album}
+                  </Card.Title>
+                  <Card.Text className="text-center fw-bold">
+                    ${product.price}
+                  </Card.Text>
+                  <Button variant="info" className="text-white fw-bold mt-2">
+                    Add To Cart
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </section>
   );
