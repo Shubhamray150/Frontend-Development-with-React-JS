f761537ac477cf53ed24b627f8b50253422efbd1
Shubham Ray <shubhamray150@gmail.com>
2025-08-04 21:32:54 +0530

    Loading the application added loading spinner

--- 07-Building-your-Ecommerce-Website/swapi APi call/swapi/src/App.jsx
+++ 07-Building-your-Ecommerce-Website/swapi APi call/swapi/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 import "./App.css";
 import Movies from "./component/Movies";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  async function buttonClickHandler() {
-    const response = await fetch("https://swapi.tech/api/films/");
+  const [isLoading, setIsLoading] = useState(false);
 
+  async function buttonClickHandler() {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.tech/api/films/");
     const data = await response.json();
     setMovies(data.result);
+    setIsLoading(false);
   }
   return (
     <>
-      <Movies data={movies} />
+      {isLoading && <BeatLoader />}
+      {!isLoading && <Movies data={movies} />}
+
       <button onClick={buttonClickHandler}>Fetch</button>
     </>
   );
