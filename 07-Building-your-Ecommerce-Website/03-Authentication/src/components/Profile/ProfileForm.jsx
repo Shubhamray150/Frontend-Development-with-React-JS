7c5b9d965612b05b5d04bb46f6e5dd086d1ffc55
Shubham Ray <shubhamray150@gmail.com>
2025-08-12 12:42:56 +0530

    added feature to updata password when user is logged in

--- 07-Building-your-Ecommerce-Website/03-Authentication/src/components/Profile/ProfileForm.jsx
+++ 07-Building-your-Ecommerce-Website/03-Authentication/src/components/Profile/ProfileForm.jsx
@@ -1,17 +1,46 @@
 import classes from "./ProfileForm.module.css";
+import { useContext, useRef } from "react";
+import AuthContext from "../../Store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
   const passwordChangeHandler = (event) => {
     console.log(event.target.value);
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDkEwWiv_6ACWxYKCBben6-sb4KbPeIB2s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: newPasswordInputRef.current.value,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        authCtx.login(data.idToken);
+        console.log(data.idToken);
+      });
+  };
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor="new-password">New Password</label>
         <input
           type="password"
           onChange={passwordChangeHandler}
           id="new-password"
+          ref={newPasswordInputRef}
         />
       </div>
       <div className={classes.action}>
