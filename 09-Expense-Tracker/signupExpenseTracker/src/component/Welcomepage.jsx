8a2a43382ba5e237b3c3103c840804fb260e1f2b
Shubhamray150 <shubhamray150@gmail.com>
2025-08-26 13:46:00 +0530

    made some changes

--- 09-Expense-Tracker/signupExpenseTracker/src/component/Welcomepage.jsx
+++ 09-Expense-Tracker/signupExpenseTracker/src/component/Welcomepage.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import HomePage from "../pages/HomePage";
 import Body from "./Body";
-import Cart from "./cart/Cart";
-import { useSelector } from "react-redux";
 
 const Welcomepage = () => {
-  const cartVisible = useSelector((state) => state.cart.cartIsVisible);
-  console.log(cartVisible);
-
   return (
     <>
       <HomePage />
-      {cartVisible && <Cart />}
+
       <Body />
     </>
   );
--- 09-Expense-Tracker/signupExpenseTracker/src/store/redux/store.jsx
+++ 09-Expense-Tracker/signupExpenseTracker/src/store/redux/store.jsx
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authReducer";
 import expenseReducer from "./expenseReducer";
-import cartReducer from "./cartreducer";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     expense: expenseReducer,
-    cart: cartReducer,
   },
 });
 export default store;
