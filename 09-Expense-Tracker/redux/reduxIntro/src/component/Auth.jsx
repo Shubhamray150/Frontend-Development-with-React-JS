1b8cbf637b4b25d18de998d57ad8c141d1ec7397
Shubhamray150 <shubhamray150@gmail.com>
2025-08-30 22:58:30 +0530

    added ref features

--- 09-Expense-Tracker/redux/reduxIntro/src/component/Auth.jsx
+++ 09-Expense-Tracker/redux/reduxIntro/src/component/Auth.jsx
@@ -1,26 +1,31 @@
 import classes from "./Auth.module.css";
 import { authActions } from "../store";
 import { useDispatch } from "react-redux";
+import { useRef } from "react";
 
 const Auth = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   const Dispatch = useDispatch();
 
   const submitHandler = (event) => {
     event.preventDefault();
+    console.log(emailRef.current.value, passwordRef.current.value);
     Dispatch(authActions.login());
   };
-  
+
   return (
     <main className={classes.auth}>
       <section>
         <form onSubmit={submitHandler}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" ref={emailRef} id="email" />
           </div>
           <div className={classes.control}>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" ref={passwordRef} id="password" />
           </div>
           <button>Login</button>
         </form>
